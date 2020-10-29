933088bc8139deae12155151e48d2f75b8db63fd
Kristen Steele <ksteelephoto@gmail.com>
2020-10-29 13:56:20 -0500

    add all components to add park card html and event listener for park details button

---
+++ scripts/parks/ParkHTML.js
@@ -0,0 +1,32 @@
+const eventHub = document.querySelector(".container")
+
+
+
+export const ParkPlaceholderHTML = () => {
+    return `
+        <h3>National Park:</h3>
+        <img src="" alt="image will go here">
+        `
+}
+
+export const ChosenParkHTML = (parkObj) => {
+    return  `
+        <div id="park-${parkObj.id}" class="card">
+            <h3>National Park:</h3>
+            <h3 class="park__name">${parkObj.fullName}</h3>
+            <button id="parkDetail--${parkObj.id}">More Info</button>
+        </div>
+        `
+}
+
+eventHub.addEventListener("click", event => {
+    const [prefix, id] = event.target.id.split("--")
+    if (prefix === "parkDetail") {
+        const customEvent = new CustomEvent("parkDetailClicked", {
+            detail: {
+                parkId: id
+            }
+        })
+        eventHub.dispatchEvent(customEvent)
+    }
+})
