e7a95b8cabfb4939b38fd137217b72c93ec49e86
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-10-29 11:39:07 -0500

    Added ParkSelect and render functions to ParkSelect.js, rendered park select dropdown to the dom and checked that selection works.

--- scripts/parks/ParkSelect.js
+++ scripts/parks/ParkSelect.js
@@ -1,14 +1,43 @@
+import { getParks, useParks } from "./ParkProvider.js"
+
 const ContentTarget = document.querySelector(".filters__park")
 const eventHub = document.querySelector(".container")
+// console.log("eventHub:", eventHub)
 
 eventHub.addEventListener("change", event => {
-    if(event.target.id === "parkSelect") {
+    if (event.target.id === "parkSelect") {
+        console.log("park chosen change:", event)
         const customEvent = new CustomEvent("parkChosen", {
-            detail: {       
-//Not sure if we need parseInt here since ID doesn't appear to be an integer         
-                parkThatWasChosen: (event.target.value) 
+            detail: {
+                //Not sure if we need parseInt here since ID doesn't appear to be an integer         
+                parkThatWasChosen: (event.target.value)
+                // console.log("park that was chosen:", parkThatWasChosen)
             }
         })
         eventHub.dispatchEvent(customEvent)
     }
 })
+
+export const ParkSelect = () => {
+    getParks()
+        .then(() => {
+            const parks = useParks()
+            // console.log("parks:", parks)
+            render(parks)
+        }
+        )
+}
+
+const render = parksCollection => {
+    ContentTarget.innerHTML = `
+    <select class="dropdown" id="parkSelect">
+        <option value="0">Select A Park</option>
+        ${parksCollection.map(
+            parksObj => {
+                return `<option value="${parksObj.id}">${parksObj.fullName}</option>`
+            }
+        ).join("")
+    }
+        </select>
+    `
+}
