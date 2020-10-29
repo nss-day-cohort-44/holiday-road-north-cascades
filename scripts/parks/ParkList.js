2a94da7f4e99ffda8fc5314e3957b18050b6a463
admin <christopherleorondeau@gmail.com>
2020-10-29 15:10:01 -0500

    added eventListener for parkChosen with render function that invokes ChosenParkHTML

--- scripts/parks/ParkList.js
+++ scripts/parks/ParkList.js
@@ -1,1 +1,34 @@
+import { ChosenParkHTML } from "./ParkHTML.js"
+import { getParks , useParks } from "./ParkProvider.js"
+
+
 const contentContainer = document.querySelector(".parkContainer")
+const eventHub = document.querySelector(".container")
+
+
+
+
+eventHub.addEventListener("parkChosen", event => {
+
+    if(event.detail.parkThatWasChosen !==0) {
+        const parksCollection = useParks()
+
+        const parkSelected = parksCollection.find(parkObj => {
+            
+            return parkObj.id === event.detail.parkThatWasChosen
+        })
+        // console.log("Park Event" , parkSelected)
+        render(parkSelected)
+    }
+})
+
+const render = (selectedPark) => {
+    
+    const parkHTMLRepresentation = ChosenParkHTML(selectedPark)
+
+    contentContainer.innerHTML = `
+     <div>${parkHTMLRepresentation}</div>`
+    
+}
+
+
