a9d91e20a9c407ca0d2aa761dba183fb872db454
admin <christopherleorondeau@gmail.com>
2020-10-30 10:56:47 -0500

    started eateryHTML module

---
+++ scripts/eateries/EateryHTML.js
@@ -0,0 +1,34 @@
+const eventHub = document.querySelector(".container")
+const contentTarget = document.querySelector(".eateryContainer")
+
+
+export const eateryPlaceholderHTML = () => {
+   contentTarget.innerHTML = `
+    
+        <h3>National eatery:</h3>
+        <img src="" alt="image will go here">
+        `
+}
+
+export const ChosenEateryHTML = (eateryObj) => {
+    return  `
+        <div id="eatery-${eateryObj.id}" class="card">
+            <h3>National Eatery:</h3>
+            <h3 class="eatery__name">${eateryObj.fullName}</h3>
+            <button id="eateryDetail--${eateryObj.id}">More Info</button>
+        </div>
+        `
+}
+
+eventHub.addEventListener("click", event => {
+    const [prefix, id] = event.target.id.split("--")
+    if (prefix === "eateryDetail") {
+        const customEvent = new CustomEvent("eateryDetailClicked", {
+            detail: {
+                eateryId: id
+            }
+        })
+        console.log("click" , event)
+        eventHub.dispatchEvent(customEvent)
+    }
+})
