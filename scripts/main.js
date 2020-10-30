cdef165b9af81412dfa8c9b84e834244cd37a89a
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-10-30 09:21:47 -0500

    added imports for attractions and eateries to main js to allow for testing.

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,22 @@
 import { ChosenParkHTML, ParkPlaceholderHTML } from "./parks/ParkHTML.js"
+import { ChosenAttractionHTML, AttractionPlaceholderHTML } from "./attractions/AttractionHTML.js"
+import { ChosenEateryHTML, EateryPlaceholderHTML } from "./eateries/EateryHTML.js"
+
+
 import { ParkSelect } from "./parks/ParkSelect.js"
+import { AttractionSelect } from "./attractions/AttractionSelect.js"
+import { EaterySelect } from "./eateries/EaterySelect.js"
+
 import "./parks/ParkList.js"
 import "./parks/ParkDetail.js"
+import "./attractions/AttractionList.js"
+import "./attractions/AttractionDetail.js"
+import "./eateries/EateryList.js"
+import "./eateries/EateryDetail.js"
+
 
 
 ParkSelect()
 ParkPlaceholderHTML()
+AttractionSelect()
+EaterySelect()
