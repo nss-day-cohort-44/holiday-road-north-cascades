80ccb6a1aa406c9e476359b9fe0787c5f48d3674
admin <christopherleorondeau@gmail.com>
2020-10-30 13:48:03 -0500

    added detail module with eventlistener for click event

---
+++ scripts/eateries/EateryDetail.js
@@ -0,0 +1,24 @@
+import { useEateries } from "./eateryProvider.js"
+
+const eventHub = document.querySelector(".container")
+
+eventHub.addEventListener("eateryDetailClicked", (event) => {
+    const arrayOfEateries = useEateries()
+
+    const foundEatery = arrayOfEateries.find((eateryObj) => {
+        return eateryObj.id === (parseInt(event.detail.eateryId))
+        
+    })
+    console.log("hey we found an eatery", foundEatery)
+    render(foundEatery)
+})
+
+const render = (eateryObj) => {
+    const contentTarget = document.querySelector(".eateryContainer")
+
+    contentTarget.innerHTML += `
+    <div class="description">
+        ${eateryObj.description}
+        </div>
+    `
+}
