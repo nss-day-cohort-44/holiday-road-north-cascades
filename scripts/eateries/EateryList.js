ffbfaf7206655560e4252135b4d245de181beb57
Kristen Steele <ksteelephoto@gmail.com>
2020-10-30 10:49:21 -0500

    add all EateryList.js components to listen for change event on eatery dropdown and render chosen eatery html to the DOM

---
+++ scripts/eateries/EateryList.js
@@ -0,0 +1,34 @@
+import { ChosenEateryHTML } from "./EateryHTML.js"
+import { getEateries , useEateries } from "./EateryProvider.js"
+
+
+const contentContainer = document.querySelector(".eateryContainer")
+const eventHub = document.querySelector(".container")
+
+
+
+
+eventHub.addEventListener("eateryChosen", event => {
+
+    if(event.detail.eateryThatWasChosen !==0) {
+        const eateriesCollection = useEateries()
+
+        const eaterySelected = eateriesCollection.find(eateryObj => {
+            
+            return eateryObj.id === event.detail.eateryThatWasChosen
+        })
+        // console.log("Park Event" , parkSelected)
+        render(eaterySelected)
+    }
+})
+
+const render = (selectedEatery) => {
+    
+    const eateryHTMLRepresentation = ChosenEateryHTML(selectedEatery)
+
+    contentContainer.innerHTML = `
+     <div>${eateryHTMLRepresentation}</div>`
+    
+}
+
+
